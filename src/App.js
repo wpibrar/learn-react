6d0030fc19143677bdb34dd19447225fbe03e370
wpibrar <wpibrar@gmail.com>
2023-10-12 04:46:08 +0500

    end of wed.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import './App.css';
-import Header from "./comps/sections/Header/Header"
+import ExpenseItem from "./components/ExpenseItem"
 
 function App() {
+
+  const Expenses = [
+    {
+      date: new Date('2023-01-05T15:30:00.000Z'),
+      amount: '$250',
+      title: 'Grosery' 
+    },
+    {
+      date: new Date('2023-01-05T15:30:00.000Z'),
+      amount: '$110',
+      title: 'Kids' 
+    },
+    {
+      date: new Date('2023-01-05T15:30:00.000Z'),
+      amount: '$125',
+      title: 'Wife' 
+    },
+    {
+      date: new Date('2023-01-05T15:30:00.000Z'),
+      amount: '$90',
+      title: 'Mobile' 
+    },
+    {
+      date: new Date('2023-01-05T15:30:00.000Z'),
+      amount: '$350',
+      title: 'Dad' 
+    },
+    {
+      date: new Date('2023-01-05T15:30:00.000Z'),
+      amount: '$200',
+      title: 'Guests'
+    }
+  ];
+
+
+
   return (
     <div className="App">
-      <Header />
-      <main>
-        <h1>Body Content</h1>
-      </main>
-      <footer>
-        <h1>Footer</h1>
-      </footer>
-    </div>
+      <ExpenseItem data={Expenses[0]} />
+    </div>      
   );
 }
 
