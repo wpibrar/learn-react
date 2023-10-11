322f730c0a07954d12b25d48377056fabfe025e3
ibrartt <111246165+ibrar19911@users.noreply.github.com>
2023-10-12 00:28:34 +0500

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./comps/sections/Header/Header"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <h1>Body Content</h1>
+      </main>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </div>
   );
 }
---
+++ src/comps/sections/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1>Header section</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
